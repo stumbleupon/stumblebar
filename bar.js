af704471dacd8c9a3f0528bf70f163b8ba96f221
Anoakie Turner <aturner@stumbleupon.com>
2017-03-01 17:50:23 -0800

    Fix CSS transitions for FF

--- bar.js
+++ bar.js
@@ -7,8 +7,8 @@ Toolbar.theme = {
   url: "toolbar.html",
 	iframe: 'position:fixed;bottom:0;left:0;display:none;' +
         	'width:288px;height:90px;z-index:2147483647;border:0;' +
-			'overflow:hidden;box-shadow: 0 0 16px -4px #000; border-radius:4px; border: 1px solid #aaa;',
-	css:    '#discoverbar { transition: height .2s,width .2s; }',
+			'overflow:hidden;box-shadow: 0 0 16px -4px #000; border-radius:4px; border: 1px solid #aaa; transition: width 0.2s, height 0.2s; ',
+	css:    '#discoverbar { transition: width 0.2s, height 0.2s; -webkit-transition-property: background-color, color; -webkit-backface-visibility: hidden; -webkit-perspective: 1000; -webkit-transform:translate3d(0,0,0); -webkit-tap-highlight-color: rgba(0,0,0,0); }',
   draggable: true,
 }
 
