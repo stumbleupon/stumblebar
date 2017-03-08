2a7b28f177fe404769db3310855624342a5c099e
Anoakie Turner <aturner@stumbleupon.com>
2017-03-07 17:57:32 -0800

    Noop on empty comments

--- toolbarevent.js
+++ toolbarevent.js
@@ -197,6 +197,9 @@ ToolbarEvent.error = function(e) {
 
 ToolbarEvent.replyConvo = function(request, sender) {
 	var convo = ToolbarEvent.api.getConversation(request.data.id);
+	// Noop if comment is empty
+	if (!request.data.comment || !request.data.comment.replace(/^[ ]+|[ ]+$/))
+		return ToolbarEvent._buildResponse({ });
 	return Promise.resolve(convo.comment(request.data.comment))
 		.then(function(comment) {
 			return ToolbarEvent._buildResponse({ comment: comment });
