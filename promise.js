389f369f07f38cf19b4a8636d49c3766038f37b4
Anoakie Turner <aturner@stumbleupon.com>
2017-03-13 15:00:38 -0700

    Cleanup

--- promise.js
+++
@@ -1,9 +0,0 @@
-function P(p) {
-	var _resolve, _reject;
-	var _promise = new Promise(function(resolve, reject) {
-		_resolve = resolve;
-		_reject = reject;
-	})
-	p.then(_resolve, _reject)
-	return _promise;
-}
